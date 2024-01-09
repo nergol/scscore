b306e606ae3b550f5ce009187cecea328886a5be
Pavel Elagin <pavel.elagin@catsci.com>
2024-01-09 23:13:10 +0000

    working app

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
+import streamlit as st
 from scscore.standalone_model_numpy import SCScorer
 scscorer = SCScorer()
 import rdkit.Chem as Chem
 from rdkit.Chem import Draw
+# Logo on top left
+st.image('./catsci-logo.svg', width=200)  # Adjust width as needed
+
+# Name of the script
+st.title('SCR-02: SCScore')  # Replace with your script name
+
+# Brief description
+st.markdown('''
+    description will be provided later
+    ''')
 
 model = SCScorer()
 model.restore()
 
-molecule = 'O=C1CC[C@@](C=C)(C=O)N1'
-
+molecule = st.text_input('Isomeric SMILES')
 (smi, score) = model.get_score_from_smi(molecule)
-print('SCScore of input molecule %.3f' % (score))
+st.write('SCScore of input molecule %.3f' % (score))
 
 m = Chem.MolFromSmiles(molecule)
 img = Draw.MolToImage(m)
-img.show()
+st.image(img)
